53d477cb422b757fccc2fa2111924279614031ad
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-11-03 14:39:57 -0400

    feat: custom document config

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const Document = () => (
+<>
+    <Html>
+        <Head>
+        <meta name="description" content="Medical prescription generator" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </Html>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+</>
+)
+
+export default Document;
