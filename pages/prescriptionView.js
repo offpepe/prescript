e23e5339cfa140c7f10eb9cc8c976ddf52d72494
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-11-06 17:03:25 -0400

    feat: view page to print

--- pages/prescriptionView.js
+++ pages/prescriptionView.js
@@ -1,11 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router'
 import { Document, Page, pdfjs } from 'react-pdf';
 
-export default function PrescriptionView ({ document }) {
+export default function PrescriptionView () {
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  const [copie, setCopie] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    const { copie: cp } = router.query;
+    setCopie(cp);   
+  },[router])
   return (
     <Document
-    file='./api/prescription/get'
+    file={ copie ?  './api/prescription/generateCopie' : './api/prescription/get' }
     onLoadError={ (err) => console.log(err)}
   >
   <Page pageNumber={ 1 } height={ 1000 } onRenderSuccess={ () => window.print() } />
