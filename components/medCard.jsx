5c832feafc663e12dbef80ef564f7a8622cab37e
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-11-12 10:01:18 -0400

    style: formated identation

--- components/medCard.jsx
+++ components/medCard.jsx
@@ -1,34 +1,29 @@
-import style from '../styles/Home.module.css';
+import style from "../styles/Home.module.css";
 import { Card } from "react-bootstrap";
 import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function MedCard ({ 
-    med: {
-    name,
-    appearance,
-    qtd,
-    },
-    id,
-    removeMed,
- }) {
-    return (
-        <Card className={ style.medCard }>
-            <Card.Body className={ style.medCardBody } >
-                <div style={ { width: '70%' } } >
-                    <p>{ `${ name } ${ appearance && appearance}`}</p>
-                </div>
-                <div style={ { width: '30%' } } >
-                    <p>{ qtd }</p>
-                </div>
-                <FontAwesomeIcon
-                  icon={ faTimesCircle }
-                  style={ { fontSize: 20, color: 'red' } }
-                  onClick= { () => removeMed(id) }
-                  width="10%"
-                  />
-
-            </Card.Body>
-        </Card>
-    )
+export default function MedCard({
+  med: { name, appearance, qtd },
+  id,
+  removeMed,
+}) {
+  return (
+    <Card className={style.medCard}>
+      <Card.Body className={style.medCardBody}>
+        <div style={{ width: "70%" }}>
+          <p>{`${name} ${appearance && appearance}`}</p>
+        </div>
+        <div style={{ width: "30%" }}>
+          <p>{qtd}</p>
+        </div>
+        <FontAwesomeIcon
+          icon={faTimesCircle}
+          style={{ fontSize: 20, color: "red" }}
+          onClick={() => removeMed(id)}
+          width="10%"
+        />
+      </Card.Body>
+    </Card>
+  );
 }
