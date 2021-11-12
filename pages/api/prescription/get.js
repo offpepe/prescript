bd1cb1cfb42a4c906171269fe71bb2223edf573f
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-11-12 10:03:29 -0400

    style: formated identation

--- pages/api/prescription/get.js
+++ pages/api/prescription/get.js
@@ -1,10 +1,10 @@
-import fs from 'fs';
+import fs from "fs";
 
-export default async function getPrescription (_req, res) {
-    try {
-    const pdf = fs.createReadStream('./public/generated/prescription.pdf');
+export default async function getPrescription(_req, res) {
+  try {
+    const pdf = fs.createReadStream("./public/generated/prescription.pdf");
     res.status(200).send(pdf);
-    } catch(err) {
-        res.status(500).json({ error: err.message })
-    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
--- pages/prescriptionView.js
+++ pages/prescriptionView.js
@@ -1,21 +1,27 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router'
-import { Document, Page, pdfjs } from 'react-pdf';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { Document, Page, pdfjs } from "react-pdf";
 
-export default function PrescriptionView () {
+export default function PrescriptionView() {
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
   const [copie, setCopie] = useState(false);
   const router = useRouter();
   useEffect(() => {
     const { copie: cp } = router.query;
-    setCopie(cp);   
-  },[router])
+    setCopie(cp);
+  }, [router]);
   return (
     <Document
-    file={ copie ?  './api/prescription/generateCopie' : './api/prescription/get' }
-    onLoadError={ (err) => console.log(err)}
-  >
-  <Page pageNumber={ 1 } height={ 1000 } onRenderSuccess={ () => window.print() } />
-  </Document>
+      file={
+        copie ? "./api/prescription/generateCopie" : "./api/prescription/get"
+      }
+      onLoadError={(err) => console.log(err)}
+    >
+      <Page
+        pageNumber={1}
+        height={1000}
+        onRenderSuccess={() => window.print()}
+      />
+    </Document>
   );
 }
